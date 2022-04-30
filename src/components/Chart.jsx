218917cbbd64f5ff87a5cbb4e582159e2e756d95
Barış SAVAŞ <barissavas17@gmail.com>
2022-05-01 01:09:26 +0300

    chart modified

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from 'react'
 import Spinner from './Spinner'
 import { useParams } from 'react-router-dom'
 import { fetchDetails } from '../api/api';
-import { Bar, Line} from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
+// eslint-disable-next-line no-unused-vars
 import { Chart as ChartJS } from 'chart.js/auto';
+import { CardMedia } from '@mui/material';
+
 
 const Chart = () => {
   const [details, setDetails] = useState([]);
   const { country } = useParams()
 
-  console.log(country)
   useEffect(() => {
     const fetchDetailsAPI = async () => {
       const initialDetails = await fetchDetails(country);
@@ -63,13 +65,46 @@ const Chart = () => {
   };
 
 
-
+  const options = {
+    title: {
+      display: true,
+      text: 'COVID-19 Cases ' + country
+    },
+    legend: {
+      display: true,
+      position: 'right',
+      labels: {
+        fontColor: 'red'
+      }
+    },
+    layout: {
+      padding: {
+        left: 250,
+        right: 50,
+        top: 50,
+        bottom: 50
+      }
+      },
+    tooltips: {
+      enabled: true
+    },
+    maintainAspectRatio: false,
+  };
 
   return (
-    <div className="chart">
+    <div className="chart"
+    style={{
+      backgroundImage: `url(${details[0]?.flag})`,
+      backgroundSize: '200px',
+      backgroundRepeat: 'no-repeat',
+      backgroundPositionX: '10px',
+      backgroundPositionY: 'center',
+    }}>
+    
       <Bar 
       data={data}  
-      redraw={true} 
+      redraw={true}
+      options={options}
       />
     </div>
   )
